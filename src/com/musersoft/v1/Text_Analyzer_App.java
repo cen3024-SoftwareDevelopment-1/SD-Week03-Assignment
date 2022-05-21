90cf9f586c26be1af63245d504f839c9494d5078
James Lee <bass-guy@cfl.rr.com>
2022-05-21 08:49:26 -0400

    Add utility function to flag non-words. Limit output to top 20. Add screenshot of results.

--- src/com/musersoft/v1/Text_Analyzer_App.java
+++ src/com/musersoft/v1/Text_Analyzer_App.java
@@ -1,4 +1,10 @@
 package com.musersoft.v1;
+/**
+* Program Name: Lee_Text_Analyzer_App
+* Purpose: Analyze text of Macbeth play. Display word frequency in descending order.
+* @author James Lee
+* @since 5/29/2022
+*/
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,32 +16,36 @@ public class Text_Analyzer_App {
 
 	public static void main(String[] args) throws IOException {
 
+		// Declare the desired lines of output
+		final int LINES_OF_OUTPUT = 20;
+		
         // File path is passed as parameter
         File file = new File("C:\\JavaProjects\\SD-Week03-Assignment\\tragedy_of_macbeth.txt");
 
         // Creating an object of BufferedReader class
         BufferedReader br = new BufferedReader(new FileReader(file));
  
+        // Declare TreeMap collection for words in play (key) and number of occurrences for each (value)
         Map<String, Integer> tm = new TreeMap<>();
 
-        // Declaring a string variable
+        // Declare a string variable to hold one line from file
         String sentence;
         
-        // Condition holds true untill there is null character in string
+        // Condition holds true until there is null character in string
         while ((sentence = br.readLine()) != null) {
  
             if (!(sentence == null || sentence.isEmpty())) {
             	
             	// User RegEx to split up sentence into words
-                String[] words = sentence.split("[\\s\\n\\t\\r\\f,;:.!--&]+");
+                //String[] words = sentence.split("[\\s+\\p{P}]");
+                String[] words = sentence.split("[\\s\\n\\t\\r\\f,;:.!--&|]+");
             	
             	// Print the sentence, one word at a time
                 for(String word : words){
-                	if (!word.isEmpty()) {
-                		// System.out.println(word);
+                	if (goodWord(word)) {
                 		
-                        // Build a map with word and word count as the key/value pairs
-                        // Increment the count if word in already in list
+                        // Add word to tree map with value initialized to 1 (key/value pairs)
+                        // ... or Increment the word count if word is already in set
                 		String key = word.toLowerCase();
                 		if (key.length() > 0) {
                 			if (!tm.containsKey(key)) {
@@ -48,19 +58,50 @@ public class Text_Analyzer_App {
                 			}
                 		}
                 	}
-                }
+                } //end for loop
             }
-        }
+        } //end While loop
 
-        // tm.forEach((k, v) -> System.out.println(k + "\t" + v));
+        // Sort the tree map by word count in descending order
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(tm.entrySet());
         Collections.sort(entries, (entry1, entry2) -> {
         	return entry2.getValue().compareTo(entry1.getValue());
         });
+        
+        // Display the desired LINES_OF_OUTPUT
+        int i = 0; 
         for (Map.Entry<String, Integer> entry: entries) {
-        	System.out.printf("%-11s %s%n", entry.getKey(), entry.getValue());
+        	if (i++ < LINES_OF_OUTPUT)
+        		System.out.printf("%-11s %s%n", entry.getKey(), entry.getValue());
         }
         
+        br.close();
+        
+	} //End main()
+	
+	
+	// Utility function for flagging non-words
+	public static boolean goodWord(String str) 
+	{
+		boolean goodWord = true;
+		
+		// Populate an array of non-words
+        String[] nonWords = {
+        	"s", "d", "o", "t", "n", "c", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii"
+        };
+        
+        // Check for empty string
+        if (str.isEmpty()) 
+    		goodWord = false;
+        
+        // Compare input against array of non-words
+        for(int i = 0; i < nonWords.length; i++) {
+        	if (str.equals(nonWords[i]))
+        		goodWord = false;
+        }
+                
+	    return goodWord; 
+	    
 	}
 
-}
+} //End class Text_Analyzer_App
